f3d81a3af3c9be6d088e31c36e323eea471fc3a0
ikuokuo <ikuokuo@hotmail.com>
2021-12-12 23:10:45 +0800

    feat: pdfjs viewer.html

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export const CONSTANTS = {
   // pdfUrl: 'http://localhost:9012/helloworld.pdf',
   pdfUrl: 'http://localhost:9012/compressed.tracemonkey-pldi-09.pdf',
+  pdfWebViewerUrl: 'http://localhost:9012/pdfjs/web/viewer.html',
 };
--- src/renderer/pages/PdfViewerHTML/index.tsx
+++ src/renderer/pages/PdfViewerHTML/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
+import { CONSTANTS } from '@constants';
+import './style.scss';
+
 export const PdfViewerHTML: React.FC = () => {
+  const { pdfUrl, pdfWebViewerUrl } = CONSTANTS;
+
   React.useEffect(() => {
     console.log('PdfViewerHTML mounted');
   });
-  return <div>PdfViewerHTML</div>;
+  return (
+    <div className="viewerHTML">
+      <div>pdfUrl={pdfUrl}</div>
+      <div>pdfWebViewerUrl={pdfWebViewerUrl}</div>
+      <iframe
+        className="pdfViewer"
+        title="PdfViewerHTML"
+        src={`${pdfWebViewerUrl}?file=${pdfUrl}`}
+      />
+    </div>
+  );
 };
